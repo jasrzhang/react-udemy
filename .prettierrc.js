6644464dcc7758f4960eb02432d8b4728c9289d3
jasrzhang <zhangshurui1989@gmail.com>
2024-06-03 23:01:41 +1000

    clean up boilerplate and start from scratch

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true, // Adds a semicolon at the end of every statement.
+  trailingComma: "es5", // Adds trailing commas wherever possible (ES5+).
+  singleQuote: true, // Use single quotes instead of double quotes.
+  printWidth: 80, // Specifies the line length that the printer will wrap on.
+  tabWidth: 2, // Specifies the number of spaces per indentation level.
+  jsxSingleQuote: false, // Use double quotes in JSX attributes.
+  bracketSpacing: true, // Print spaces between brackets in object literals.
+  arrowParens: "avoid", // Omit parens when possible. Example: `x => x`.
+  jsxBracketSameLine: false, // Puts the `>` of a multi-line JSX element at the end of the last line instead of being alone on the next line.
+  endOfLine: "lf", // Use line feed only (\n) as line ending.
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Greeting() {
+  return (
+    <div>
+      <h3>hello people</h3>
+      <ul>
+        <li>
+          <a href="#">hello world</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Greeting />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default Greeting;
